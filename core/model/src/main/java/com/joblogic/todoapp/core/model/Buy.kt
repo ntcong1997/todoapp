f5382c22c01a302cf713a22c745fa8ba5bb9e833
cong <ntcong1997@gmail.com>
2023-03-03 22:24:27 +0700

    feat: init model module

---
+++ core/model/src/main/java/com/joblogic/todoapp/core/model/Buy.kt
@@ -0,0 +1,12 @@
+package com.joblogic.todoapp.core.model
+
+/**
+ * Created by TC on 03/03/2023.
+ */
+
+data class Buy(
+    val id: String?,
+    val name: String?,
+    val price: Long?,
+    val quantity: Int?
+)
---
+++ core/model/src/main/java/com/joblogic/todoapp/core/model/Call.kt
@@ -0,0 +1,11 @@
+package com.joblogic.todoapp.core.model
+
+/**
+ * Created by TC on 03/03/2023.
+ */
+
+data class Call(
+    val id: String?,
+    val name: String?,
+    val number: String?
+)
